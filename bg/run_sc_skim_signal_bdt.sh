6b8e234685df70cabb1e37ba3ad398f333418a96
Yuval <nissan.yuval@gmail.com>
2019-08-26 20:52:20 +0200

    fixed simulation

---
+++ bg/run_sc_skim_signal_bdt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. "/afs/desy.de/user/n/nissanuv/cms-tools/bg/def.sh"
+
+$BG_DIR/run_bg_skim_signal_bdt.sh -sc
---
+++ sim_x10x20x10/run_sc_skim_signal_bdt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. "/afs/desy.de/user/n/nissanuv/cms-tools/bg/def.sh"
+
+$SIM_DIR/run_skim_signal_bdt.sh -sc
