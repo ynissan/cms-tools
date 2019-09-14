cee3b09a83e9df2e45b6021564339dbb2cadb5b7
Yuval <nissan.yuval@gmail.com>
2019-09-14 03:56:39 +0200

    fixing bug

--- Production/test/condorSub/jobSubmitterCT.py
+++ Production/test/condorSub/jobSubmitterCT.py
@@ -2,18 +2,26 @@ from Condor.Production.jobSubmitter import *
 from glob import glob
 import os
 
+defaultModeLocations: {
+    "def" : "srm://dcache-se-cms.desy.de/pnfs/desy.de/cms/tier2/store/user/ynissan/NtupleHub/def/"
+}
+
 class jobSubmitterCT(jobSubmitter):
     def addExtraOptions(self,parser):
         super(jobSubmitterCT,self).addExtraOptions(parser)
         self.removeOptions(parser,"-m")
         print "Parser after remove:", parser
         parser.add_option("-m", "--mode", dest="mode", default="", help="mode to run (required) (default = %default)")
+        parser.add_option("-o", "--output", dest="output", default="", help="path to output directory in which root files will be stored (required) (default = %default)"
     
     def checkExtraOptions(self,options,parser):
         super(jobSubmitterCT,self).checkExtraOptions(options,parser)
         
         if len(options.mode)==0 or options.mode not in ("def", "aod", "miniaod", "ntuples"):
             parser.error("Required option: --mode [mode]")
+        
+        if len(options.output)==0:
+            options.output = defaultModeLocations[options.mode]
     
     def generateExtra(self,job):
         super(jobSubmitterCT,self).generateExtra(job)
