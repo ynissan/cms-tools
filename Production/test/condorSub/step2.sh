57eef4c005493a2f718fd66e27a5208884fb2537
Yuval <nissan.yuval@gmail.com>
2019-09-16 01:28:23 +0200

    dev cms-connect submit

--- Production/test/condorSub/step2.sh
+++ Production/test/condorSub/step2.sh
@@ -60,7 +60,11 @@ if [[ "$MODE" == "def" ]]; then
     echo Runnning: cmsDriver.py ${ARGS[0]} --datamix PreMix --conditions auto:run2_mc --pileup_input dbs:/RelValFS_PREMIXUP15_PU25/CMSSW_9_4_11_cand2-PU25ns_94X_mcRun2_asymptotic_v3_FastSim-v1/GEN-SIM-DIGI-RAW --fast --era Run2_2016 --eventcontent AODSIM --relval 100000,1000 -s GEN,SIM,RECOBEFMIX,DIGIPREMIX_S2:pdigi_valid,DATAMIX,L1,DIGI2RAW,L1Reco,RECO --datatier AODSIM --beamspot Realistic50ns13TeVCollision --python_filename=${ARGS[1]} --fileout ${ARGS[2]} --no_exec -n 500 --customise SimGeneral/DataMixingModule/customiseForPremixingInput.customiseForPreMixingInput
     cmsDriver.py ${ARGS[0]} --datamix PreMix --conditions auto:run2_mc --pileup_input dbs:/RelValFS_PREMIXUP15_PU25/CMSSW_9_4_11_cand2-PU25ns_94X_mcRun2_asymptotic_v3_FastSim-v1/GEN-SIM-DIGI-RAW --fast --era Run2_2016 --eventcontent AODSIM --relval 100000,1000 -s GEN,SIM,RECOBEFMIX,DIGIPREMIX_S2:pdigi_valid,DATAMIX,L1,DIGI2RAW,L1Reco,RECO --datatier AODSIM --beamspot Realistic50ns13TeVCollision --python_filename=${ARGS[1]} --fileout ${ARGS[2]} --no_exec -n 500 --customise SimGeneral/DataMixingModule/customiseForPremixingInput.customiseForPreMixingInput
 elif [[ "$MODE" == "aod" ]]; then
-    cmd="cmsRun ${ARGS[0]}"
+    COPY_CMD="gfal-copy ${ARGS[0]} ."
+    echo "Running: $COPY_CMD"
+    $COPY_CMD
+    export basename_name=$(basename ${ARGS[0]})
+    cmd="cmsRun " $basename_name
     echo "Running: $cmd"
     $cmd
 fi
@@ -103,7 +107,7 @@ if [[ "$MODE" == "def" ]]; then
     ${CMDSTR} -n 1 ${ARGS[1]} ${OUTDIR}/
     rm ${ARGS[1]}
 elif [[ "$MODE" == "aod" ]]; then
-    echo Running: ${CMDSTR} -n 1 ${ARGS[1]} ${OUTDIR}/
-    ${CMDSTR} -n 1 ${ARGS[0]} ${OUTDIR}/
-    rm ${ARGS[0]}
+    echo Running: ${CMDSTR} -n 1 $basename_name ${OUTDIR}/
+    ${CMDSTR} -n 1 $basename_name ${OUTDIR}/
+    rm $basename_name
 fi
