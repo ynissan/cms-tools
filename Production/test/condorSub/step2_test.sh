19c0aa34721beeb490933f4342d1cb51f5f843b5
Yuval <nissan.yuval@gmail.com>
2019-09-26 18:11:13 +0200

    dev cms login

--- Production/test/condorSub/step2_test.sh
+++ Production/test/condorSub/step2_test.sh
@@ -64,9 +64,17 @@ if [[ ( "$CMSSITE" == "T1_US_FNAL" && "$USER" == "cmsgli" && "${OUTDIR}" == *"ro
     export OUTDIR=${GSIFTP_ENDPOINT}${OUTDIR#root://cmseos.fnal.gov//store/user/}
 fi
 
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMSSW_BASE/src/cms-tools/lib/classes
+cd $CMSSW_BASE/src/cms-tools/lib/classes
+rm LumiSectMap_C.so
+echo .L LumiSectMap.C+ | root.exe -b
+cd $CMSSW_BASE/src/cms-tools/analysis/scripts/
+
 FILE="Summer16.QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1AOD_60000-A8B482FE-C9B0-E611-9FD1-A0000420FE80_RA2AnalysisTree.root"
 
 echo ${CMDSTR} -n 1 srm://dcache-se-cms.desy.de/pnfs/desy.de/cms/tier2/store/user/ynissan/NtupleHub/ProductionRun2v3/$FILE .
+${CMDSTR} -n 1 srm://dcache-se-cms.desy.de/pnfs/desy.de/cms/tier2/store/user/ynissan/NtupleHub/ProductionRun2v3/$FILE .
 
 echo "Running locally"
 echo $CMSSW_BASE/src/cms-tools/analysis/scripts/skimmer_x1x2x1.py -i $FILE -o tmp.root -bg
