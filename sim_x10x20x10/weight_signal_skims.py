d2877c37bdc724e689a326d883c509f0cbf2c113
Quintus Diepholz <quintus.diepholz@studium.uni-hamburg.de>
2022-05-27 12:13:14 +0200

    cms-tools -> stops

--- sim_x10x20x10/weight_signal_skims.py
+++ sim_x10x20x10/weight_signal_skims.py
@@ -11,8 +11,8 @@ import cppyy
 import itertools
 from datetime import datetime
 
-sys.path.append(os.path.expandvars("$CMSSW_BASE/src/cms-tools"))
-sys.path.append(os.path.expandvars("$CMSSW_BASE/src/cms-tools/lib/classes"))
+sys.path.append(os.path.expandvars("$CMSSW_BASE/src/stops"))
+sys.path.append(os.path.expandvars("$CMSSW_BASE/src/stops/lib/classes"))
 from lib import analysis_ntuples
 from lib import analysis_tools
 from lib import utils
