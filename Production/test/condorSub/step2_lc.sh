f0462325feb0edc7d08a4792e83f6d76c260e9b9
Yuval <nissan.yuval@gmail.com>
2022-08-08 18:34:40 -0500

    fixing lc

--- Production/test/condorSub/step2_lc.sh
+++ Production/test/condorSub/step2_lc.sh
@@ -56,13 +56,22 @@ if [[ $vomsident = *"cmsgli"* ]]; then
     exit 60322
 fi
 
+ORIG_CMSSW=$CMSSW_BASE
+
 # copy output to eos
 echo "prepare gfal tools"
 #if [ -e "/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.3/current/el6-x86_64/setup.sh" ]; then
-if [ -e "/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el7-x86_64/setup.sh" ]; then
+#if [ -e "/cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el7-x86_64/setup.sh" ]; then
     #. /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.3/current/el6-x86_64/setup.sh
-    . /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el7-x86_64/setup.sh
-fi      
+#    . /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el7-x86_64/setup.sh
+#fi
+
+. /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el7-x86_64/setup.sh
+export PYTHONHOME=/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/python/2.7.15/
+. /cvmfs/grid.cern.ch/umd-c7ui-latest/etc/profile.d/setup-c7-ui-example.sh
+cd $ORIG_CMSSW
+cmsenv
+      
 export CMDSTR="gfal-copy"
 export GFLAG=""
 
@@ -84,8 +93,8 @@ fi
 for f in ${ARGS[*]}; do
     echo "Processing " $f
     echo "Running ${CMDSTR} -n 1 $INDIR/$f ."
-    (eval `scram unsetenv -sh`; ${CMDSTR} -n 1 $INDIR/$f .)
-    #${CMDSTR} -n 1 $INDIR/$f .
+    #(eval `scram unsetenv -sh`; ${CMDSTR} -n 1 $INDIR/$f .)
+    ${CMDSTR} -n 1 $INDIR/$f .
     mkdir tmp
     echo "Running ./create_lepton_collection.py -i $f -o tmp/$f"
     ./create_lepton_collection.py -i $f -o tmp/$f
@@ -97,8 +106,8 @@ for f in ${ARGS[*]}; do
         continue
     fi
     echo "Running ${CMDSTR} -n 1 tmp/$f ${OUTDIR}/"
-    (eval `scram unsetenv -sh`; ${CMDSTR} -f tmp/$f ${OUTDIR}/)
-    #${CMDSTR} -f tmp/$f ${OUTDIR}/
+    #(eval `scram unsetenv -sh`; ${CMDSTR} -f tmp/$f ${OUTDIR}/)
+    ${CMDSTR} -f tmp/$f ${OUTDIR}/
     if [[ $? -ne 0 ]]; then
         echo "Deleting file because gfal-copy failed"
         echo gfal-rm ${OUTDIR}/$f
